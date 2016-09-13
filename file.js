25fc381e35f76532cce1b5485ce5d47c58086992
Matthew Solomonson <ggmatt@gmail.com>
2016-09-13 12:01:31 -0400

    test js

---
+++ file.js
@@ -0,0 +1,31 @@
+import 'babel-polyfill'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { geoPath, geoAlbersUsa } from 'd3-geo'
+import { select } from 'd3-selection'
+import { json } from 'd3-request'
+import { feature } from 'topojson'
+import topo from '../build/topo-counties.json'
+
+const projection = geoAlbersUsa()
+
+// convert topojson back to geojson
+const counties = feature(topo, topo.objects.counties)
+
+const path = geoPath().projection(projection)
+const App = () => {
+  const data = path({
+    type: "FeatureCollection",
+    features: counties.features,
+  })
+   return (
+    <svg width={1000} height={500}>
+      <path d={data} fill={'orange'} stroke={'black'} />
+    </svg>
+   )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('target'),
+)
